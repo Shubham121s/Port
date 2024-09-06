b43b750fe8e3b73bad7894ee0a97021dc0bd2a62
Shubham121s <sp6063512@gmail.com>
2024-09-06 11:49:15 +0530

    change

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -102,7 +102,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="_blank"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy;2024 Shubham.All Rights Reserved.
+          &copy;2024 Shubham.All rights reserved.
         </Copyright>
       </FooterWrapper>
     </FooterContainer>
