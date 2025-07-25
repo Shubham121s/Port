7cc1079eeb206963ad996081976f7817620dba8d
Shubham121s <sp6063512@gmail.com>
2025-07-25 13:46:17 +0530

    upload

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
 import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
-import HeroImg1 from '../../images/HeroImage1.jpg'
+import HeroImg1 from '../../images/bj.jpg'
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
 
